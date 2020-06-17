66a9eb28263bfd6bee4adf3a3c6f32e1c15f26f1
Amra Sezairi <amra.sezairi@hotmail.com>
2020-06-17 12:29:10 -0400

    Methods and Conditionals

---
+++ Methods/index.rb
@@ -0,0 +1,17 @@
+def average_of_three(num1, num2, num3)
+    sum = num1+num2+num3 
+    avg = sum /3.0
+    return avg;
+end
+
+puts average_of_three(3, 7, 8)   # => 6.0
+puts average_of_three(2, 5, 17)  # => 8.0
+puts average_of_three(2, 8, 1)   # => 3.666666
+
+def goodbye(name)
+	return "Bye " + name +".";
+end
+
+puts goodbye("Daniel")   # => "Bye Daniel."
+puts goodbye("Mark")     # => "Bye Mark."
+puts goodbye("Beyonce")  # => "Bye Beyonce."
